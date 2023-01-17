6d766370a19976cc9ab89ebbb308e15b1312abba
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2023-01-17 18:03:18 +0530

    Geolocation ✅

---
+++ app/geolocation.controller.js
@@ -0,0 +1,44 @@
+angular.module("swiggy").controller("geolocation", [
+  "$scope",
+  "$http",
+  "$timeout",
+  "$location",
+  function ($scope, $http, $timeout, $location) {
+    $scope.location = "";
+    $scope.lat = "";
+    $scope.long = "";
+    $scope.getlocation = function () {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+          $scope.lat = position.coords.latitude;
+          $scope.long = position.coords.longitude;
+          let { latitude, longitude } = position.coords;
+          //   $scope.location = `Latitude: ${$scope.lat} Longitude: ${$scope.long}`;
+          $http({
+            method: "GET",
+            url: `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${`b2aa0f9f2a660c0e008c6a949fde67fb`}&units=metric&lang=en`,
+          })
+            .then((response) => response.data)
+            .then((name) => {
+              console.log(name);
+              var fulllocation = `${name.city.name}, ${name.city.country}`;
+              $scope.location = fulllocation;
+              localStorage.setItem("location", fulllocation);
+              console.log(fulllocation);
+              $location.path("/menu");
+              //   window.location.href = "#/menu";
+              //   document.getElementById("location").innerHTML = fulllocation;
+              //   $scope.$apply();
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+          console.log($scope.location);
+          $scope.$apply();
+        });
+      } else {
+        $scope.location = "Geolocation is not supported by this browser.";
+      }
+    };
+  },
+]);
--- app/typeanimation.js
+++ app/typeanimation.js
@@ -1,20 +1,47 @@
-angular.module("swiggy").controller("animationType", [
-  "$scope",
-  function ($scope) {
-    $scope.type = function (e) {
-      var text = e.target.value;
-      var textArray = text.split("");
-      var textArrayLength = textArray.length;
-      var i = 0;
-      var timer = setInterval(function () {
-        if (i < textArrayLength) {
-          $scope.text = $scope.text + textArray[i];
-          $scope.$apply();
-          i++;
-        } else {
-          clearInterval(timer);
-        }
-      }, 100);
-    };
-  },
-]);
+angular.module("swiggy").controller("animationType", function ($scope) {
+  $scope.animcontent = "chal ra hai";
+
+  var arr = [
+    "Hungry?",
+    "Movie marathon?",
+    "Unexpected guests?",
+    "Late Night at Office?",
+    "Cooking gone wrong?",
+    "Game Night?",
+    "Late Night at Office?",
+  ];
+
+  var i = 0;
+  let count = 0;
+  let slideInterval;
+
+  function typeWriter() {
+    handleOnNextClick();
+    slideInterval = setInterval(() => {
+      handleOnNextClick();
+    }, 3000);
+  }
+  typeWriter();
+
+  function handleOnNextClick() {
+    count = (count + 1) % arr.length;
+    $scope.animcontent = arr[count];
+  }
+
+  $scope.type = function (e) {
+    var text = e.target.value;
+    var textArray = text.split("");
+    var textArrayLength = textArray.length;
+    var i = 0;
+    $scope.text = "";
+    var timer = setInterval(function () {
+      if (i < textArrayLength) {
+        $scope.text = $scope.text + textArray[i];
+        $scope.$apply();
+        i++;
+      } else {
+        clearInterval(timer);
+      }
+    }, 100);
+  };
+});
