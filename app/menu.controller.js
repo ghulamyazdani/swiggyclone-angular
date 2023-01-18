3a348ff127ff9d88efaafbd4e8806ba7f96507e5
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2023-01-18 16:08:21 +0530

    menu controller , menu ui , search ,sort, filter ✅

---
+++ app/menu.controller.js
@@ -0,0 +1,198 @@
+angular.module("swiggy").controller("menu", [
+  "$scope",
+  "$http",
+  "$timeout",
+  "$location",
+  function ($scope, $http, $timeout, $location) {
+    $scope.placeList = [
+      {
+        id: 52977,
+        mealImg:
+          "https://www.themealdb.com/images/media/meals/58oia61564916529.jpg",
+        name: "Corba",
+        country: "Turkish",
+        price: 100,
+        deliverytime: "30 min",
+        rating: 4,
+        promoted: true,
+        foodItems:
+          "Bakery, Desserts, Beverages, Combo, European, Ice Cream, Juices, Waffle, Sweets.",
+      },
+      {
+        id: 53060,
+        mealImg:
+          "https://www.themealdb.com/images/media/meals/tkxquw1628771028.jpg",
+        name: "Burek",
+        country: "Croatian",
+        price: 100,
+        deliverytime: "30 min",
+        rating: 4.5,
+        promoted: true,
+
+        foodItems:
+          "Bakery, Desserts, Beverages, Combo, European, Ice Cream, Juices, Waffle, Sweets.",
+      },
+      {
+        id: 53065,
+        mealImg:
+          "https://www.themealdb.com/images/media/meals/g046bb1663960946.jpg",
+        name: "Burek",
+        country: "Croatian",
+        price: 100,
+        deliverytime: "30 min",
+        promoted: false,
+
+        rating: 4.5,
+        foodItems:
+          "Bakery, Desserts, Beverages, Combo, European, Ice Cream, Juices, Waffle, Sweets.",
+      },
+      {
+        id: 52978,
+        mealImg:
+          "https://www.themealdb.com/images/media/meals/mlchx21564916997.jpg",
+        name: "Kumpir",
+        country: "Turkish",
+        price: 100,
+        deliverytime: "30 min",
+        promoted: false,
+
+        rating: "4.5",
+        foodItems:
+          "Bakery, Desserts, Beverages, Combo, European, Ice Cream, Juices, Waffle, Sweets.",
+      },
+      {
+        id: 53026,
+        mealImg:
+          "https://www.themealdb.com/images/media/meals/tkxquw1628771028.jpg",
+        name: "Tamiya",
+        country: "Egyptian",
+        price: 100,
+        deliverytime: "30 min",
+        promoted: false,
+
+        rating: "4.5",
+        foodItems:
+          "Bakery, Desserts, Beverages, Combo, European, Ice Cream, Juices, Waffle, Sweets.",
+      },
+      {
+        id: 52785,
+        mealImg:
+          "https://www.themealdb.com/images/media/meals/wuxrtu1483564410.jpg",
+        name: "Dal fry",
+        country: "Indian",
+        price: 100,
+        deliverytime: "30 min",
+        rating: 4.5,
+        promoted: false,
+
+        foodItems:
+          "Bakery, Desserts, Beverages, Combo, European, Ice Cream, Juices, Waffle, Sweets.",
+      },
+      {
+        id: 52804,
+        mealImg:
+          "https://www.themealdb.com/images/media/meals/uuyrrx1487327597.jpg",
+        name: "Poutine",
+        country: "Canadian",
+        price: 100,
+        deliverytime: "30 min",
+        rating: 4.5,
+        promoted: false,
+
+        foodItems:
+          "Bakery, Desserts, Beverages, Combo, European, Ice Cream, Juices, Waffle, Sweets.",
+      },
+      {
+        id: 52844,
+        mealImg:
+          "https://www.themealdb.com/images/media/meals/wtsvxx1511296896.jpg",
+        name: "Lasagne",
+        country: "Italian",
+        price: 200,
+        deliverytime: "30 min",
+        promoted: false,
+        rating: 3.5,
+        foodItems:
+          "Bakery, Desserts, Beverages, Combo, European, Ice Cream, Juices, Waffle, Sweets.",
+      },
+    ];
+    $scope.mealsCount = $scope.placeList.length;
+    $scope.cart = JSON.parse(localStorage.getItem("meals")) || [];
+    $scope.cartCount = $scope.cart.length || 0;
+    $scope.menuData = "";
+    $scope.recentOrders = [];
+    $scope.sortColumn = "name";
+    $scope.reverseSort = false;
+    $scope.searchText = "";
+    $scope.location = localStorage.getItem("location");
+    $scope.user = JSON.parse(localStorage.getItem("user"));
+    if ($scope.user === null) {
+      $location.path("/");
+    }
+    $scope.addMealToLS = function (mealId, mealName, mealImg) {
+      // const meal = getMealsFromLS();
+
+      const mealItem = {
+        id: mealId,
+        name: mealName,
+        img: mealImg,
+      };
+
+      $scope.cart.push(mealItem);
+      // console.log(meal);
+      localStorage.setItem("meals", JSON.stringify($scope.cart));
+      LoadCartMeal();
+    };
+    $scope.placeorder = function () {
+      let recent = JSON.parse(localStorage.getItem("meals"));
+
+      localStorage.setItem(
+        "recentmeals",
+        recent.map((e) => e.name)
+      );
+      $scope.recentOrders = recent.map((e) => e.name);
+
+      console.log("clicked");
+      localStorage.setItem("meals", JSON.stringify([]));
+      setCartCount(0);
+      $scope.cart = [];
+    };
+    function setCartCount(count) {
+      $scope.cartCount = count;
+    }
+    $scope.sortway = "";
+    $scope.sortData = function (column, status) {
+      if (column === "price" && status == true) {
+        $scope.sortway = "HTL";
+        $scope.sortColumn = column;
+      } else if (column === "price" && status == false) {
+        $scope.sortColumn = column;
+        $scope.sortway = "LTH";
+      } else {
+        $scope.sortColumn = column;
+        $scope.sortway = column;
+      }
+      $scope.reverseSort = status;
+    };
+    $scope.getSortClass = function (column, way) {
+      if (column === "rating" && $scope.sortway === "rating") {
+        return "active";
+      }
+      if (column === "price") {
+        console.log(way, $scope.sortway);
+        return way == $scope.sortway ? "active" : "not-active";
+      }
+      if ($scope.sortColumn == column) {
+        return $scope.sortway ? "not-active" : "active";
+      }
+
+      return "";
+    };
+    function LoadCartMeal() {
+      const meals = JSON.parse(localStorage.getItem("meals"));
+      if (meals) {
+        $scope.cartCount = meals.length;
+      }
+    }
+  },
+]);
--- scripts/foodfetcher.js
+++ scripts/foodfetcher.js
@@ -1,166 +1,166 @@
-// const mealsEl = document.getElementById("meals");
-// const searchBtn = document.getElementById("search");
-// const searchTerm = document.getElementById("search-term");
-// const cartMeal = document.getElementById("modalbody");
-// getMealMount();
+const mealsEl = document.getElementById("meals");
+const searchBtn = document.getElementById("search");
+const searchTerm = document.getElementById("search-term");
+const cartMeal = document.getElementById("modalbody");
+getMealMount();
 
-// window.onload = () => {
-//   // document.getElementById("loading").style.display = "none";
-//   const meals = JSON.parse(localStorage.getItem("meals"));
-//   const recentmeals = localStorage.getItem("recentmeals");
-//   if (recentmeals) {
-//     document.getElementById("recOrders").innerHTML = recentmeals;
-//   }
-//   if (meals) {
-//     document.getElementById("cartCount").innerHTML = meals.length;
-//   }
-//   const location = localStorage.getItem("location");
-//   if (location) {
-//     document.getElementById("locationShow").innerHTML = location;
-//   }
-//   console.log(meals);
-//   if (meals) {
-//     meals.forEach((meal) => {
-//       addCartMeal(meal);
-//     });
-//   }
-// };
+window.onload = () => {
+  // document.getElementById("loading").style.display = "none";
+  const meals = JSON.parse(localStorage.getItem("meals"));
+  const recentmeals = localStorage.getItem("recentmeals");
+  if (recentmeals) {
+    document.getElementById("recOrders").innerHTML = recentmeals;
+  }
+  if (meals) {
+    document.getElementById("cartCount").innerHTML = meals.length;
+  }
+  const location = localStorage.getItem("location");
+  if (location) {
+    document.getElementById("locationShow").innerHTML = location;
+  }
+  console.log(meals);
+  if (meals) {
+    meals.forEach((meal) => {
+      addCartMeal(meal);
+    });
+  }
+};
 
-// function LoadCartMeal() {
-//   const meals = JSON.parse(localStorage.getItem("meals"));
-//   if (meals) {
-//     document.getElementById("cartCount").innerHTML = meals.length;
-//   }
-// }
-// searchBtn.addEventListener("click", async () => {
-//   // clean container
-//   mealsEl.innerHTML = "";
+function LoadCartMeal() {
+  const meals = JSON.parse(localStorage.getItem("meals"));
+  if (meals) {
+    document.getElementById("cartCount").innerHTML = meals.length;
+  }
+}
+searchBtn.addEventListener("click", async () => {
+  // clean container
+  mealsEl.innerHTML = "";
 
-//   getMealMount();
-// });
-// async function getMealMount() {
-//   const search = searchTerm.value || "";
-//   const meals = await getMealsBySearch(search);
+  getMealMount();
+});
+async function getMealMount() {
+  const search = searchTerm.value || "";
+  const meals = await getMealsBySearch(search);
 
-//   if (meals) {
-//     meals.forEach((meal) => {
-//       addMeal(meal);
-//     });
-//   }
-// }
+  if (meals) {
+    meals.forEach((meal) => {
+      addMeal(meal);
+    });
+  }
+}
 
-// async function getMealsBySearch(term) {
-//   const resp = await fetch(
-//     "https://www.themealdb.com/api/json/v1/1/search.php?s=" + term
-//   );
+async function getMealsBySearch(term) {
+  const resp = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/search.php?s=" + term
+  );
 
-//   const respData = await resp.json();
-//   const meals = respData.meals;
+  const respData = await resp.json();
+  const meals = respData.meals;
 
-//   return meals;
-// }
+  return meals;
+}
 
-// function addMeal(mealData) {
-//   console.log(mealData);
+function addMeal(mealData) {
+  console.log(mealData);
 
-//   const meal = document.createElement("div");
-//   meal.classList.add("meal");
+  const meal = document.createElement("div");
+  meal.classList.add("meal");
 
-//   meal.innerHTML = `
-//           <div class="meal-header">
-//               <img
-//                   src="${mealData.strMealThumb}"
-//                   alt="${mealData.strMeal}"
-//               />
-//           </div>
-//           <div class="meal-body">
-//               <h4>${mealData.strMeal}</h4>
-//               <button class="add-btn">
-//                   <i class="fas fa-plus"></i> Add to cart
-//               </button>
-//           </div>
-//       `;
+  meal.innerHTML = `
+          <div class="meal-header">
+              <img
+                  src="${mealData.strMealThumb}"
+                  alt="${mealData.strMeal}"
+              />
+          </div>
+          <div class="meal-body">
+              <h4>${mealData.strMeal}</h4>
+              <button class="add-btn">
+                  <i class="fas fa-plus"></i> Add to cart
+              </button>
+          </div>
+      `;
 
-//   const btn = meal.querySelector(".add-btn");
-//   btn.addEventListener("click", () => {
-//     addMealToLS(mealData.idMeal, mealData.strMeal, mealData.strMealThumb);
-//     LoadCartMeal();
-//   });
+  const btn = meal.querySelector(".add-btn");
+  btn.addEventListener("click", () => {
+    addMealToLS(mealData.idMeal, mealData.strMeal, mealData.strMealThumb);
+    LoadCartMeal();
+  });
 
-//   mealsEl.appendChild(meal);
-// }
+  mealsEl.appendChild(meal);
+}
 
-// function addCartMeal(mealData) {
-//   console.log(mealData);
+function addCartMeal(mealData) {
+  console.log(mealData);
 
-//   const meal = document.createElement("div");
-//   meal.classList.add("cartItem");
+  const meal = document.createElement("div");
+  meal.classList.add("cartItem");
 
-//   meal.innerHTML = `
-//               <img
-//                 style="
-//                   width: 100px;
-//                   height: 100px;
-//                   object-fit: cover;
-//                   margin-right: 1rem;
-//                 "
-//                 src="${mealData.img}"
-//                 alt=""
-//               />
-//               <div class="itemText">
-//                 <h5>${mealData.name}</h5>
-//                 <div>
-//                   <p>1 item</p>
-//                 </div>
-//               </div>
-//       `;
+  meal.innerHTML = `
+              <img
+                style="
+                  width: 100px;
+                  height: 100px;
+                  object-fit: cover;
+                  margin-right: 1rem;
+                "
+                src="${mealData.img}"
+                alt=""
+              />
+              <div class="itemText">
+                <h5>${mealData.name}</h5>
+                <div>
+                  <p>1 item</p>
+                </div>
+              </div>
+      `;
 
-//   cartMeal.appendChild(meal);
-// }
+  cartMeal.appendChild(meal);
+}
 
-// function addMealToLS(mealId, mealName, mealImg) {
-//   const meal = getMealsFromLS();
+function addMealToLS(mealId, mealName, mealImg) {
+  const meal = getMealsFromLS();
 
-//   const mealItem = {
-//     id: mealId,
-//     name: mealName,
-//     img: mealImg,
-//   };
+  const mealItem = {
+    id: mealId,
+    name: mealName,
+    img: mealImg,
+  };
 
-//   meal.push(mealItem);
-//   console.log(meal);
-//   document.getElementById("cartCount").innerHTML = meal.length;
-//   localStorage.setItem("meals", JSON.stringify(meal));
-// }
+  meal.push(mealItem);
+  console.log(meal);
+  document.getElementById("cartCount").innerHTML = meal.length;
+  localStorage.setItem("meals", JSON.stringify(meal));
+}
 
-// function getMealsFromLS() {
-//   const meals = JSON.parse(localStorage.getItem("meals"));
+function getMealsFromLS() {
+  const meals = JSON.parse(localStorage.getItem("meals"));
 
-//   return meals === null ? [] : meals;
-// }
+  return meals === null ? [] : meals;
+}
 
-// document.getElementById("placeorder").addEventListener("click", () => {
-//   let recent = JSON.parse(localStorage.getItem("meals"));
+document.getElementById("placeorder").addEventListener("click", () => {
+  let recent = JSON.parse(localStorage.getItem("meals"));
 
-//   localStorage.setItem(
-//     "recentmeals",
-//     recent.map((e) => e.name)
-//   );
-//   document.getElementById("recOrders").innerHTML = recent.map((e) => e.name);
+  localStorage.setItem(
+    "recentmeals",
+    recent.map((e) => e.name)
+  );
+  document.getElementById("recOrders").innerHTML = recent.map((e) => e.name);
 
-//   console.log("clicked");
-//   localStorage.setItem("meals", JSON.stringify([]));
-//   setCartCount(0);
-// });
+  console.log("clicked");
+  localStorage.setItem("meals", JSON.stringify([]));
+  setCartCount(0);
+});
 
-// function setCartCount(count) {
-//   document.getElementById("cartCount").innerHTML = count;
-// }
+function setCartCount(count) {
+  document.getElementById("cartCount").innerHTML = count;
+}
 
-// function handleFilterOpen() {
-//   if (document.getElementById("filterBox").style.display === "none") {
-//     document.getElementById("filterBox").style.display = "flex";
-//   } else {
-//     document.getElementById("filterBox").style.display = "none";
-//   }
-// }
+function handleFilterOpen() {
+  if (document.getElementById("filterBox").style.display === "none") {
+    document.getElementById("filterBox").style.display = "flex";
+  } else {
+    document.getElementById("filterBox").style.display = "none";
+  }
+}
